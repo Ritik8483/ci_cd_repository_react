449cb6bedffceaad2f779bd0e63d6e037abb8441
Ritik Chauhan <ritchauh3@WKMZT15494A7.global.publicisgroupe.net>
2025-12-12 21:06:56 +0530

    (feat): add code in deploy dist file

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
     <>
       <h1>Vite + React</h1>
       <p>
-        Click on the Vite and React logos to learn more Ritik
+        Click on the Vite and React logos to learn more
       </p>
     </>
   )
